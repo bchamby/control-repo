245edb600df49a45eddcd870c8c53860e9f6b354
Brad Chamberlain <chamby@gmail.com>
2016-11-30 22:23:35 -0500

    added ip addr to mysql conf

--- site/profile/manifests/mysql.pp
+++ site/profile/manifests/mysql.pp
@@ -1,4 +1,14 @@
 # site/profile/manifests/mysql.pp
 class profile::mysql {
-  include ::mysql::server
+  # include ::mysql::server
+
+  $override_options = {
+    'mysqld' => {
+      'bind_address' => $::networking['interfaces']['eth1']['ip'],
+    }
+  }
+
+  class { '::mysql::server':
+    override_options => $override_options,
+  }
 }
