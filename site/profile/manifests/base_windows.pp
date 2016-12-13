0c72a610e33490c534435fbf8b7dccec1dc00d01
Brad Chamberlain <chamby@gmail.com>
2016-12-13 12:55:04 -0500

    adding dhcp script to windows base profile

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -1,3 +1,14 @@
 class profile::base_windows {
-  
+
+  $script = @(EOF)
+    $nicd = (gwmi win32_networkadapterconfiguration | ? {$_.ipaddress -like "192.168.50.*"}).description
+    $nic = get-netadapter | ? {$_.interfacedescription -eq $nicd}
+    $nic | set-netipinterface -dhcp enabled
+    | EOF
+
+    exec { 'set-dhcpenabled':
+      command => $script,
+      provider => powershell,
+    }
+    
 }
