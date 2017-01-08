4fcb3ae6146562987edb6ac2a445c32931207be5
Brad Chamberlain <chamby@gmail.com>
2017-01-08 09:26:57 -0500

    commenting out dhcp script for now

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -1,14 +1,14 @@
 class profile::base_windows {
 
-  $script = @(EOF)
-    $nicd = (gwmi win32_networkadapterconfiguration | ? {$_.ipaddress -like "192.168.50.*"}).description
-    $nic = get-netadapter | ? {$_.interfacedescription -eq $nicd}
-    $nic | set-netipinterface -dhcp enabled
-    | EOF
+  # $script = @(EOF)
+  #   $nicd = (gwmi win32_networkadapterconfiguration | ? {$_.ipaddress -like "192.168.50.*"}).description
+  #   $nic = get-netadapter | ? {$_.interfacedescription -eq $nicd}
+  #   $nic | set-netipinterface -dhcp enabled
+  #   | EOF
+  #
+  #   exec { 'set-dhcpenabled':
+  #     command => $script,
+  #     provider => powershell,
+  #   }
 
-    exec { 'set-dhcpenabled':
-      command => $script,
-      provider => powershell,
-    }
-    
 }
